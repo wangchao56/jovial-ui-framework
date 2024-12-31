830fc955df69e2e27ca7b7fecffee39f9dadf154
jieni <18571351438@163.com>
2025-01-01 00:30:36 +0800

    实现space间距组件

---
+++ packages/components/space/src/space.ts
@@ -0,0 +1,37 @@
+import { ExtractPropTypes, PropType } from 'vue'
+
+export type Align = 'start' | 'end' | 'center' | 'baseline' | 'stretch'
+export type Direction = 'horizontal' | 'vertical'
+export type Size = 'small' | 'medium' | 'large' | number | [number, number]
+export type Justify =
+  | 'start'
+  | 'end'
+  | 'center'
+  | 'between'
+  | 'around'
+  | 'evenly'
+export const spaceProps = {
+  inline: Boolean,
+  wrap: Boolean,
+  size: {
+    type: String as PropType<Size>,
+    values: ['x-small', 'small', 'medium', 'large', 'x-large'],
+    default: 'medium'
+  },
+  direction: {
+    type: String as PropType<Direction>,
+    values: ['horizontal', 'vertical'],
+    default: 'horizontal'
+  },
+  justify: {
+    type: String as PropType<Justify>,
+    values: ['start', 'end', 'center', 'between', 'around', 'evenly'],
+    default: 'start'
+  },
+  align: {
+    type: String as PropType<Align>,
+    values: ['start', 'end', 'center', 'baseline', 'stretch'],
+    default: 'start'
+  }
+} as const
+export type SpaceProps = ExtractPropTypes<typeof spaceProps>
