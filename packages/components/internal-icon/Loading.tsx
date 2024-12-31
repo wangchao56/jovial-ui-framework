99d5449756dd856bc394d947bd826fbadda946ca
jieni <18571351438@163.com>
2025-01-01 00:29:09 +0800

    删除loading组件的宽高

--- packages/components/internal-icon/Loading.tsx
+++ packages/components/internal-icon/Loading.tsx
@@ -6,9 +6,7 @@ export default defineComponent({
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        height="24px"
         viewBox="0 -960 960 960"
-        width="24px"
         fill="#5f6368"
         class="loading"
       >
